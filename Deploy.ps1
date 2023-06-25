d89369a75468703f66256b208953cdf889b99240
R.Keelan <R.Keelan@gmail.com>
2023-06-25 13:27:05 -0400

    Fix file paths in Deploy.ps1

--- Deploy.ps1
+++ Deploy.ps1
@@ -1,13 +1,16 @@
 # Copy the website
 
-$srcFolder = "Site"
-$destFolder = "rkeelan"
+$root = "C:\Users\RKeelan\OneDrive\Writing\Website"
+$srcFolder = Join-Path $root "Site"
+$destFolder = Join-Path $root "rkeelan"
+
 $zipFile = "rkeelan.zip"
+$zipPath = Join-Path $root zipFile
 
 # Create the zip archive
 Copy-Item $srcFolder $destFolder -recurse -force
 Add-Type -Assembly System.IO.Compression.FileSystem
-[System.IO.Compression.ZipFile]::CreateFromDirectory($destFolder, $zipFile)
+[System.IO.Compression.ZipFile]::CreateFromDirectory($destFolder, $zipPath)
 Remove-Item -Recurse -Force rkeelan
 
 # Copy to the server
@@ -15,5 +18,5 @@ $username = "richato4"
 $hostname = "rkeelan.com"
 $userhost = "$username@$hostname"
 
-scp $zipFile $userhost":"$zipFile
-Remove-Item $zipFile
+scp $zipPath $userhost":"$zipFile
+Remove-Item $zipPath
